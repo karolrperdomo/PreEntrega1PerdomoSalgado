d1a189d5c28d7d5ad47afa7728f3e76bedb33b36
Karol Perdomo <karol.rperdomo@gmail.com>
2024-01-29 22:00:12 -0500

    Se corrige conectividad con el Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCDZ2SvsPmkYR7IvtXvP8_yLqPqY3Wxw6g",
-  authDomain: "entrega-final-comision-49905.firebaseapp.com",
-  projectId: "entrega-final-comision-49905",
-  storageBucket: "entrega-final-comision-49905.appspot.com",
-  messagingSenderId: "668368112267",
-  appId: "1:817955764694:web:6b1d60b4cb2f084a84cc77"
+  apiKey: "AIzaSyAc7WUUpbwCC5r0yDDOOf6maNjCC_X-G8U",
+  authDomain: "final-comision-49905.firebaseapp.com",
+  projectId: "final-comision-49905",
+  storageBucket: "final-comision-49905.appspot.com",
+  messagingSenderId: "199707259849",
+  appId: "1:199707259849:web:d7991341f42deb3edf2e18",
+  measurementId: "G-KCLERNV2VV"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
